b6365572ad85ee3996ddbbb51e598162c1942514
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2020-01-03 18:56:09 -0300

    Conversão de Class para Função / Utilização de Hooks

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -30,7 +29,24 @@ import { numberToBRL } from '../../utils/format';
 
 import * as CartActions from '../../store/module/Cart/actions';
 
-function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
+export default function Cart() {
+  const dispatch = useDispatch();
+
+  const cart = useSelector(state =>
+    state.cart.map(product => ({
+      ...product,
+      subtotal: numberToBRL(product.price * product.amount),
+    }))
+  );
+
+  const total = useSelector(state =>
+    numberToBRL(
+      state.cart.reduce((totall, product) => {
+        return totall + product.price * product.amount;
+      }, 0)
+    )
+  );
+
   return (
     <Container>
       <CartContainer
@@ -44,13 +60,21 @@ function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
                 <CartTitle>{item.title}</CartTitle>
                 <CartPrice>{item.priceFormatted}</CartPrice>
               </CartInfo>
-              <CartDeleteButton onPress={() => removeFromCart(item.id)}>
+              <CartDeleteButton
+                onPress={() => dispatch(CartActions.removeFromCart(item.id))}
+              >
                 <Icon name="delete" size={24} color="#000" />
               </CartDeleteButton>
             </CartHeader>
             <CartAmountView>
               <CartAmountCount>
-                <TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() =>
+                    dispatch(
+                      CartActions.updateAmountRequest(item.id, item.amount - 1)
+                    )
+                  }
+                >
                   <Icon
                     name="remove-circle-outline"
                     color="#7159c1"
@@ -58,7 +82,13 @@ function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
                   />
                 </TouchableOpacity>
                 <CartAmount value={String(item.amount)} editable={false} />
-                <TouchableOpacity>
+                <TouchableOpacity
+                  onPress={() =>
+                    dispatch(
+                      CartActions.updateAmountRequest(item.id, item.amount + 1)
+                    )
+                  }
+                >
                   <Icon name="add-circle-outline" color="#7159c1" size={24} />
                 </TouchableOpacity>
               </CartAmountCount>
@@ -79,23 +109,3 @@ function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
     </Container>
   );
 }
-
-const mapStateToProps = state => ({
-  cart: state.cart.map(product => ({
-    ...product,
-    subtotal: numberToBRL(product.price * product.amount),
-  })),
-  total: numberToBRL(
-    state.cart.reduce((total, product) => {
-      return total + product.price * product.amount;
-    }, 0)
-  ),
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Cart);
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -21,16 +20,19 @@ import {
   ButtonText,
 } from './styles';
 
-class Home extends Component {
-  state = {
-    product: [],
-  };
+export default function Home() {
+  const dispatch = useDispatch();
+  const amount = useSelector(state =>
+    state.cart.reduce((amountt, product) => {
+      amountt[product.id] = product.amount;
 
-  async componentDidMount() {
-    this.getProducts();
-  }
+      return amountt;
+    }, {})
+  );
 
-  getProducts = async () => {
+  const [products, setProducts] = useState([]);
+
+  async function getProducts() {
     try {
       const response = await api.get('products');
 
@@ -39,63 +41,47 @@ class Home extends Component {
         priceFormatted: numberToBRL(product.price),
       }));
 
-      this.setState({ product: data });
-    } catch (err) {}
-  };
-
-  handleAddProduct = id => {
-    const { addToCartRequest } = this.props;
-
-    addToCartRequest(id);
-  };
-
-  render() {
-    const { product } = this.state;
-    const { amount } = this.props;
-    return (
-      <Container>
-        <ProductList
-          data={product}
-          keyExtractor={item => String(item.id)}
-          renderItem={({ item }) => {
-            return (
-              <ProductContainer>
-                <ProductImage source={{ uri: item.image }} />
-                <ProductTitle>{item.title}</ProductTitle>
-                <ProductAmount>{item.priceFormatted}</ProductAmount>
-
-                <ButtonView
-                  type="button"
-                  onPress={() => this.handleAddProduct(item.id)}
-                >
-                  <ButtonIconView>
-                    <Icon name="shopping-cart" size={18} color="#fff" />
-                    <ButtonText>{amount[item.id] || 0}</ButtonText>
-                  </ButtonIconView>
-
-                  <ButtonText>ADICIONAR AO CARRINHO</ButtonText>
-                </ButtonView>
-              </ProductContainer>
-            );
-          }}
-        />
-      </Container>
-    );
+      setProducts(data);
+    } catch (err) {
+      // err
+    }
   }
+
+  useEffect(() => {
+    getProducts();
+  });
+
+  function handleAddProduct(id) {
+    dispatch(CartActions.addToCartRequest(id));
+  }
+
+  return (
+    <Container>
+      <ProductList
+        data={products}
+        keyExtractor={item => String(item.id)}
+        renderItem={({ item }) => {
+          return (
+            <ProductContainer>
+              <ProductImage source={{ uri: item.image }} />
+              <ProductTitle>{item.title}</ProductTitle>
+              <ProductAmount>{item.priceFormatted}</ProductAmount>
+
+              <ButtonView
+                type="button"
+                onPress={() => handleAddProduct(item.id)}
+              >
+                <ButtonIconView>
+                  <Icon name="shopping-cart" size={18} color="#fff" />
+                  <ButtonText>{amount[item.id] || 0}</ButtonText>
+                </ButtonIconView>
+
+                <ButtonText>ADICIONAR AO CARRINHO</ButtonText>
+              </ButtonView>
+            </ProductContainer>
+          );
+        }}
+      />
+    </Container>
+  );
 }
-
-const mapStateToProps = state => ({
-  amount: state.cart.reduce((amount, product) => {
-    amount[product.id] = product.amount;
-
-    return amount;
-  }, {}),
-});
-
-const mapDispatchToProps = dispatch =>
-  bindActionCreators(CartActions, dispatch);
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Home);
